572671d756f315c9ea17d68e1b5a7e8ba01fce13
arkar20 <arkar20011@gmail.com>
2025-01-25 13:05:54 -0800

    refactor: startTransition and checkbox

--- components/templates/tasks/TodoList.tsx
+++ components/templates/tasks/TodoList.tsx
@@ -7,51 +7,87 @@ import { TrashIcon } from "@/components/svgs/TrashIcon";
 import { Task } from "@/entities";
 import { cn } from "@/utils";
 import Link from "next/link";
-import React from "react";
+import React, { useTransition } from "react";
 
 type TodoProps = {
     tasks: Task[];
 };
 
 export function TodoList({ tasks }: TodoProps) {
-    const sortedTasks = tasks.sort((a, b) =>
-        a.completed === b.completed ? 0 : a.completed ? 1 : -1
-    );
+    const [isPending, startTransition] = useTransition();
+
     return (
-        <div className="flex flex-col space-y-4">
-            {sortedTasks.map((task) => (
-                <Tile
-                    key={task.id}
-                    className="pl-14 pr-16 md:pr-20"
-                    startIcon={
-                        <Checkbox
-                            isChecked={task.completed}
-                            handleChange={() =>
-                                toggleTodoStatus(task.id, !task.completed)
-                            }
-                        />
-                    }
-                    endIcon={
-                        <button
-                            type="button"
-                            onClick={() => deleteTodo(task.id)}
-                        >
-                            <TrashIcon />
-                        </button>
-                    }
-                >
-                    <Link
-                        href={`/tasks/${task.id}`}
-                        className={cn(
-                            "text-sm md:text-[14px] p-[0.5px] w-full inline-block",
-                            {
-                                "line-through text-[#808080]": task.completed,
-                            }
-                        )}
+        <div className="flex flex-col space-y-4 ">
+            <div className="absolute top-6 right-0">
+                {isPending && (
+                    <svg
+                        className="mr-3 -ml-1 size-5 animate-spin text-primary"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
                     >
-                        {task.title}
-                    </Link>
-                </Tile>
+                        <circle
+                            className="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            strokeWidth="4"
+                        ></circle>
+                        <path
+                            className="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
+                    </svg>
+                )}
+            </div>
+            {tasks.map((task) => (
+                <div key={task.id}>
+                    <Tile
+                        key={task.id}
+                        className="pl-14 pr-16 md:pr-20"
+                        startIcon={
+                            <Checkbox
+                                checked={task.completed}
+                                onChange={() => {
+                                    startTransition(async () => {
+                                        await toggleTodoStatus(
+                                            task.id,
+                                            !task.completed
+                                        );
+                                    });
+                                }}
+                            />
+                        }
+                        endIcon={
+                            <button
+                                type="button"
+                                onClick={() => {
+                                    startTransition(async () => {
+                                        await deleteTodo(task.id);
+                                    });
+                                }}
+                                disabled={isPending}
+                            >
+                                <TrashIcon />
+                            </button>
+                        }
+                    >
+                        <Link
+                            href={`/tasks/${task.id}`}
+                            className={cn(
+                                "text-sm md:text-[14px] p-[0.5px] w-full inline-block",
+                                {
+                                    "line-through text-[#808080]":
+                                        task.completed,
+                                }
+                            )}
+                        >
+                            {task.title}
+                        </Link>
+                    </Tile>
+                </div>
             ))}
         </div>
     );
